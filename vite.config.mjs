933fe8d72e8150b87bd602bf0235e66e6c9e86bd
santosh kumar sah <santoshkumar816102@gmail.com>
2025-11-20 16:47:59 +0530

    final portfolio

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 this fixes the blank page on deployment
+  base: './',               // ← YEH LINE SABSE ZAROORI HAI!!!
+  build: {
+    outDir: 'dist',         // confirm karne ke liye
+    assetsDir: 'assets'     // yeh bhi daal de
+  }
 })
